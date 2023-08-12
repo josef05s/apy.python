82ee08008920bdaf9989d7c0157be9d7d8e82f99
Richard <richard.yanqui@upeu.edu.pe>
2023-08-11 22:47:54 -0500

    as

--- app/routers/post.py
+++ app/routers/post.py
@@ -10,7 +10,7 @@ router = APIRouter(prefix="/posts",tags=['POST'])
 @router.get("/",response_model= List[schemas.Post])
 def get_posts(db: Session = Depends(database.get_db), current_user:int=Depends(oauth2.get_current_user),
               search:Optional[str] = "", limit:int=10 , skip:int=0):
-    posts = db.query(models.Post).filter(models.Post.title.owner_id==current_user.id).limit(limit).offset(skip).all()
+    posts = db.query(models.Post).filter(models.Post.owner_id==current_user.id).limit(limit).offset(skip).all()
     return posts
 
 @router.get("/{id}", response_model = schemas.Post)
@@ -40,23 +40,25 @@ def update(post: schemas.PostCreate,id:int,db: Session = Depends(database.get_db
     if post_recovery == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} does not exist")
-    post_query.update(post.dict(),synchronize_session=False)
-    db.commit()
     if post.owner_id != current_user.id:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                             detail="Not authorized to perform requested action")
+    post_query.update(post.dict(),synchronize_session=False)
+    db.commit()
+    
     return post_query.first()
 
 @router.delete("/{id}",response_model=schemas.Post)
 def delete(id:int,db: Session = Depends(database.get_db), current_user:int=Depends(oauth2.get_current_user)):
-    post = db.query(models.Post).filter(models.Post.id == id)
-    if post.first() == None:
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
+    if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} does not exist")
-    post.delete(synchronize_session=False)
-    db.commit()
     if post.owner_id != current_user.id:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                             detail="Not authorized to perform requested action")
     
+    post_query.delete(synchronize_session=False)
+    db.commit()
     return{"data":f'delete succefull posts number {id}'}
